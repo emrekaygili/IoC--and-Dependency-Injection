1daec779fa47f421b734ca0d0b565cb16d230bc6
Emre Kaygılı <kaygiliemre@gmail.com>
2024-10-03 17:24:39 +0300

    .

---
+++ src/springIntro/CustomerManager.java
@@ -0,0 +1,23 @@
+package springIntro;
+
+public class CustomerManager implements ICustomerService {
+	
+	private ICustomerDal customerDal;
+	
+	//constructor injection
+	//public CustomerManager(ICustomerDal customerDal) {
+		//this.customerDal = customerDal;
+	//}
+
+	//setter injection
+	public void setCustomerDal(ICustomerDal customerDal) {
+		this.customerDal = customerDal;
+	}
+
+	public void add() {
+		//iþ kurallarý
+		customerDal.add();
+		
+	}
+
+}
---
+++ src/springIntro/Main.java
@@ -0,0 +1,24 @@
+package springIntro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+	    
+		ICustomerService customerService = context.getBean("service",ICustomerService.class);
+		
+
+		customerService.add();
+		
+	}
+	
+	//IoC - Inversion of Control
+	//Dependency Injection
+	//Spagetti
+	//SOLID
+
+}
